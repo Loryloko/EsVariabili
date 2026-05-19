80c44ce167f49ab3675af2e2e998b1d474cf0ad6
Lorenzo <Lorenzomem56@gmail.com>
2026-05-19 17:43:10 +0200

    Es3

---
+++ Esercizio3/Nuova cartella/script.js
@@ -0,0 +1,10 @@
+let num1 = parseInt(prompt("inserisci il primo numero"))
+let num2 = parseInt(prompt("inserisci il secondo numero"))
+
+let somma = num1 + num2
+let sottrazione = num1 - num2
+let prodotto = num1 * num2
+let divisione = num1 / num2
+let potenza = num1 ** num2
+
+console.log(`Con i numeri da te scelti, i risultati delle varie operazioni sono: somma ${somma}, sottrazione ${sottrazione}, moltiplicazione ${prodotto}, divisione ${divisione}, potenza ${potenza}`)
