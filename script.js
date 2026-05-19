ca6a4a74d7e055df6a4341d16eab5322288b409d
Lorenzo <Lorenzomem56@gmail.com>
2026-05-19 13:15:52 +0200

    Variabili

---
+++ script.js
@@ -0,0 +1,13 @@
+let num1 = 1;
+let num2 = 2;
+
+console.log(num1)
+console.log(num2)
+
+let stringa = "10"
+console.log(stringa)
+
+const PIGRECO = 3.14159265359
+console.log(PIGRECO)
+
+PIGRECO = 15
